baaa080b75d38c96d7e57e2d886b27220487182e
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-06-02 00:06:36 +0000

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -37,3 +37,5 @@ if __name__ == "__main__":
             print("*", end="")
         print()  # Print a newline after each row
         row += 1
+
+size_str = input("Enter the size of the pattern: ")
